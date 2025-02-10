242b0004e87a4d782014f2cb48d764a55c19eab0
Tony Gong <gongt@google.com>
2025-02-10 12:28:59 -0800

    major changes to squash and expand

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,19 @@
 #[deny(unused_results)]
 use anyhow::{anyhow, bail, Context as _};
 use argh::FromArgs;
-use i3ipc::reply::{Command, CommandOutcome, Node, NodeLayout, NodeType};
+use i3ipc::reply::{Command, CommandOutcome, Node, NodeLayout, NodeType, WindowProperty};
 use i3ipc::I3Connection;
 use log::{debug, info};
 
 trait NodeExt {
     fn get_focused_child(&self) -> Option<(usize, &Self)>;
+    fn kind(&self) -> WindowKind;
     fn percentages_cumulative(&self) -> Vec<f64>;
     fn percentages(&self) -> Vec<f64>;
     fn widths(&self) -> Vec<i32>;
     fn right_x(&self) -> Vec<i32>;
+    fn resize_stats(&self) -> ResizeStats;
+    fn left_right(&self, i: usize) -> (Vec<ResizeStats>, Vec<ResizeStats>);
 }
 
 impl NodeExt for Node {
@@ -21,6 +24,46 @@ impl NodeExt for Node {
                 .enumerate()
                 .find(|(_, node)| node.id == id)
         })
+    }
+
+    fn kind(&self) -> WindowKind {
+        if let Some(properties) = self.window_properties.as_ref() {
+            let Some(class) = properties.get(&WindowProperty::Class) else {
+                return WindowKind::Browser;
+            };
+            if class == "Alacritty" {
+                return WindowKind::Terminal;
+            }
+            return WindowKind::Browser;
+        }
+        if self
+            .nodes
+            .iter()
+            .all(|node| node.kind() == WindowKind::Terminal)
+        {
+            return WindowKind::Terminal;
+        }
+        return WindowKind::Browser;
+    }
+
+    fn resize_stats(&self) -> ResizeStats {
+        ResizeStats {
+            percentage: self.percent.expect("missing percentage"),
+            kind: self.kind(),
+        }
+    }
+
+    fn left_right(&self, i: usize) -> (Vec<ResizeStats>, Vec<ResizeStats>) {
+        let left_windows = self.nodes[..i]
+            .iter()
+            .map(NodeExt::resize_stats)
+            .collect::<Vec<_>>();
+        let right_windows = self.nodes[i + 1..]
+            .iter()
+            .rev()
+            .map(NodeExt::resize_stats)
+            .collect::<Vec<_>>();
+        (left_windows, right_windows)
     }
 
     fn percentages_cumulative(&self) -> Vec<f64> {
@@ -126,14 +169,22 @@ fn log_tree(indent: &str, node: &Node) {
         name,
         rect,
         percent,
+        window_properties,
         nodetype,
         focused,
         layout,
         ..
     } = node;
     debug!(
-        "{}name={:?}, percent={:?}, rect={:?}, nodetype={:?}, focused={}, layout={:?}",
-        indent, name, percent, rect, nodetype, focused, layout
+        "{}name={:?}, percent={:?}, window_properties={:?}, rect={:?}, nodetype={:?}, focused={}, layout={:?}",
+        indent,
+        name,
+        percent,
+        window_properties,
+        rect,
+        nodetype,
+        focused,
+        layout
     );
     let indent = indent.to_owned() + "  ";
     for node in node.nodes.iter() {
@@ -156,6 +207,7 @@ struct Args {
 enum SubCommands {
     LogTree(LogTree),
     Resize(Resize),
+    ResizeAll(ResizeAll),
     Swap(Swap),
     Rotate(Rotate),
     Center(Center),
@@ -179,6 +231,18 @@ struct Rotate {
 #[derive(FromArgs, Debug)]
 #[argh(subcommand, name = "resize")]
 struct Resize {
+    /// direction of the percentages provided
+    #[argh(option)]
+    direction: Option<Direction>,
+    /// the new percentage of the window (uses per-window-kind default if not provided)
+    #[argh(option)]
+    percentage: Option<f64>,
+}
+
+/// ResizeAll subcommand.
+#[derive(FromArgs, Debug)]
+#[argh(subcommand, name = "resize-all")]
+struct ResizeAll {
     /// direction of the percentages provided
     #[argh(option, default = "Direction::Right")]
     direction: Direction,
@@ -241,8 +305,9 @@ impl std::fmt::Display for Direction {
     }
 }
 
-fn resize(
+fn resize_all(
     conn: &mut I3Connection,
+    // Left means left-to-right.
     dir: Direction,
     mut dst_percentages: Vec<f64>,
 ) -> Result<(), anyhow::Error> {
@@ -257,33 +322,65 @@ fn resize(
     let n = root_container.nodes.len();
     let total_width: i32 = root_container.widths().into_iter().sum();
 
-    let sum: f64 = dst_percentages.iter().sum();
-    if sum > 1.0 {
-        bail!("percentages must sum to <= 1: {:?}", dst_percentages);
+    // i3 can report percentages that sum to slightly more than 1, so
+    // we need to renormalize here.
+    let mut sum: f64 = dst_percentages.iter().sum();
+    if sum > 1.01 {
+        bail!("percentages must sum to <= 1.01: {:?}", dst_percentages);
     }
+    if sum > 1. {
+        for i in dst_percentages.iter_mut() {
+            *i = *i / sum;
+        }
+        sum = 1.;
+    }
+
     if dst_percentages.len() > n {
         bail!("too many percentages passed: {:?}", dst_percentages);
     } else if dst_percentages.len() < n {
         let omitted_count = n - dst_percentages.len();
-        let omitted = (1.0 - sum) / omitted_count as f64;
-        if omitted == 0f64 {
-            bail!("omitted percentage is 0");
-        }
         for _ in 0..omitted_count {
-            dst_percentages.push(omitted);
+            dst_percentages.push((1.0 - sum) / omitted_count as f64);
         }
     }
     if dir == Direction::Right {
         dst_percentages.reverse();
     }
+
+    let mut dst_pixels = dst_percentages
+        .into_iter()
+        .map(|i| {
+            // Don't use trunc here, it just makes things worse!
+            let i = (i * total_width as f64) as i32;
+            if i == 0 {
+                1
+            } else {
+                i
+            }
+        })
+        .collect::<Vec<i32>>();
+    let mut extra = dst_pixels.iter().copied().sum::<i32>() - total_width;
+    if extra > 0 {
+        for i in dst_pixels.iter_mut() {
+            if *i == 1 {
+                continue;
+            }
+            let take = std::cmp::min(*i - 1, extra);
+            *i -= take;
+            extra -= take;
+            if extra == 0 {
+                break;
+            }
+        }
+    }
     // dst needs to have in pixels, where the right edge of each column should
     // be (except for the rightmost column).
     let dst = {
         let mut acc = 0;
-        let mut dst = dst_percentages
+        let mut dst = dst_pixels
             .into_iter()
             .map(|x| {
-                acc += (x * total_width as f64) as i32;
+                acc += x;
                 acc
             })
             .collect::<Vec<_>>();
@@ -335,15 +432,160 @@ fn resize(
     Ok(())
 }
 
-fn center(conn: &mut I3Connection, dir: Option<Direction>) -> Result<(), anyhow::Error> {
+#[derive(Debug, Eq, PartialEq, Copy, Clone)]
+enum WindowKind {
+    Terminal,
+    Browser,
+}
+
+struct ResizeStats {
+    percentage: f64,
+    kind: WindowKind,
+}
+
+// The windows are assumed to always be listed from left-to-right. On the right
+// side the slice should be reversed before being passed in.
+//
+// Returns the new percentages and the actual amount adjusted.
+//
+// Precondition: if delta > 0, the growing must be possible
+fn adjust(windows: &mut [ResizeStats], config: Config, delta: f64) -> (Vec<f64>, f64) {
+    if delta > 0. {
+        (grow(windows, config, delta), delta)
+    } else if delta < 0. {
+        let (widths, adjusted) = shrink(windows, -delta);
+        (widths, -adjusted)
+    } else {
+        panic!("no point making a zero adjustment");
+    }
+}
+
+fn grow(windows: &mut [ResizeStats], config: Config, mut delta: f64) -> Vec<f64> {
+    // Go right to left and increase up to desired percentage. If there is leftover,
+    // give it all to the leftmost window.
+    for window in windows.iter_mut().rev() {
+        if window.percentage < config.desired(window.kind) {
+            let can_grow = delta.min(config.desired(window.kind) - window.percentage);
+            window.percentage += can_grow;
+            delta -= can_grow;
+            if delta == 0. {
+                break;
+            }
+        }
+    }
+    if delta > 0. {
+        windows[0].percentage += delta;
+    }
+    windows.iter().map(|stats| stats.percentage).collect()
+}
+
+// `delta` is allowed to be larger than the combined percentage of `windows`.
+//
+// `delta` must be positive, and the returned value is also positive.
+fn shrink(windows: &mut [ResizeStats], mut delta: f64) -> (Vec<f64>, f64) {
+    let mut adjusted = 0.;
+    // Take from left to right.
+    if delta > 0. {
+        for window in windows.iter_mut() {
+            let can_shrink = window.percentage.min(delta);
+            window.percentage -= can_shrink;
+            adjusted += can_shrink;
+            delta -= can_shrink;
+            if delta == 0. {
+                break;
+            }
+        }
+    }
+    (
+        windows.iter().map(|stats| stats.percentage).collect(),
+        adjusted,
+    )
+}
+
+fn resize(
+    conn: &mut I3Connection,
+    config: Config,
+    dir: Option<Direction>,
+    percentage: Option<f64>,
+) -> Result<(), anyhow::Error> {
+    if percentage == Some(0.) {
+        return Ok(());
+    }
     let root = conn.get_tree()?;
     let root = find_root_container(&root)?;
     let (i, _) = root
         .get_focused_child()
         .ok_or_else(|| anyhow!("no top-level container apparently"))?;
+    let focused = &root.nodes[i];
+    let focused_percent = focused.percent.expect("percent missing");
+    let (mut left_windows, mut right_windows) = root.left_right(i);
+    let mut delta = percentage.unwrap_or_else(|| config.desired(focused.kind()) - focused_percent);
+
+    if delta < 0. && -delta > focused_percent {
+        delta = -focused_percent;
+    }
+
+    let percentages = match dir {
+        None => {
+            if i == 0 || i == root.nodes.len() - 1 {
+                bail!("cannot resize centered on left or rightmost container");
+            }
+            let (left_percentages, l) = adjust(left_windows.as_mut_slice(), config, -delta / 2.);
+            let (right_percentages, r) = adjust(right_windows.as_mut_slice(), config, -delta / 2.);
+            left_percentages
+                .into_iter()
+                .chain(std::iter::once(focused_percent - l - r))
+                .chain(right_percentages.into_iter().rev())
+                .collect::<Vec<_>>()
+        }
+        Some(Direction::Left) => {
+            if i == 0 {
+                bail!("cannot resize left on leftmost container");
+            }
+            let (left_percentages, adjustment) =
+                adjust(left_windows.as_mut_slice(), config, -delta);
+            left_percentages
+                .into_iter()
+                .chain(std::iter::once(focused_percent - adjustment))
+                .chain(
+                    root.nodes[i + 1..]
+                        .iter()
+                        .map(|node| node.percent.expect("percent missing")),
+                )
+                .collect()
+        }
+        Some(Direction::Right) => {
+            if i == root.nodes.len() - 1 {
+                bail!("cannot resize right on rightmost container");
+            }
+            let (right_percentages, adjustment) =
+                adjust(right_windows.as_mut_slice(), config, -delta);
+            root.nodes[..i]
+                .iter()
+                .map(|node| node.percent.expect("percent missing"))
+                .chain(std::iter::once(focused_percent - adjustment))
+                .chain(right_percentages.into_iter().rev())
+                .collect()
+        }
+    };
+
+    resize_all(conn, Direction::Left, percentages)
+}
+
+fn center(
+    conn: &mut I3Connection,
+    config: Config,
+    dir: Option<Direction>,
+) -> Result<(), anyhow::Error> {
+    let root = conn.get_tree()?;
+    let root = find_root_container(&root)?;
+    let (i, _) = root
+        .get_focused_child()
+        .ok_or_else(|| anyhow!("no top-level container apparently"))?;
+    let focused_percent = root.nodes[i].percent.expect("percent missing");
     // Figure out if we need to move left or right.
     let percentages_cumulative = root.percentages_cumulative();
-    let mut percentages = root.percentages();
+    let percentages = root.percentages();
     let con_center = match dir {
         None => {
             if i == 0 {
@@ -367,32 +609,36 @@ fn center(conn: &mut I3Connection, dir: Option<Direction>) -> Result<(), anyhow:
             percentages_cumulative[i]
         }
     };
+    let (mut left_windows, mut right_windows) = root.left_right(i);
     let center = percentages.iter().sum::<f64>() / 2.0;
-    if (con_center * 100.0).round() == (center * 100.0).round() {
+    let (left, right) = if (con_center * 100.0).round() == (center * 100.0).round() {
         return Ok(());
     } else if con_center > center {
+        // Shrink left and grow right.
         let from_center = con_center - center;
-        percentages[0] = if percentages[0] > from_center {
-            percentages[0] - from_center
-        } else {
-            0.01
-        };
+        let (left, shrunk) = shrink(left_windows.as_mut_slice(), from_center);
+        let right = grow(right_windows.as_mut_slice(), config, shrunk);
+        (left, right)
     } else {
+        // Shrink right and grow left.
         let from_center = center - con_center;
-        let last = *percentages.last().unwrap();
-        percentages[0] += if last > from_center {
-            from_center
-        } else {
-            last - 0.01
-        };
-    }
-    // Pop the last percentage and let resize figure it out.
-    percentages.pop();
-
+        let (right, shrunk) = shrink(right_windows.as_mut_slice(), from_center);
+        let left = grow(left_windows.as_mut_slice(), config, shrunk);
+        (left, right)
+    };
+    let mut percentages: Vec<_> = left
+        .into_iter()
+        .chain(std::iter::once(focused_percent))
+        .chain(right.into_iter().rev())
+        .collect();
     debug!("performing resize: {:?}", percentages);
 
+    // NB: Pop the last percentage and let resize figure it out because i3
+    // reports sum of percentages to be >1.
+    percentages.pop();
+
     // Perform the resize.
-    resize(conn, Direction::Left, percentages)
+    resize_all(conn, Direction::Left, percentages)
 }
 
 fn focused_and(root: &Node, direction: Direction) -> anyhow::Result<(&Node, &Node)> {
@@ -478,6 +724,30 @@ fn rotate(conn: &mut I3Connection, Rotate { direction }: Rotate) -> anyhow::Resu
     Ok(())
 }
 
+#[derive(Clone, Copy, PartialEq)]
+struct Config {
+    terminal: f64,
+    browser: f64,
+}
+
+impl std::default::Default for Config {
+    fn default() -> Self {
+        Self {
+            terminal: 0.16,
+            browser: 0.25,
+        }
+    }
+}
+
+impl Config {
+    fn desired(&self, kind: WindowKind) -> f64 {
+        match kind {
+            WindowKind::Terminal => self.terminal,
+            WindowKind::Browser => self.browser,
+        }
+    }
+}
+
 fn main() -> Result<(), anyhow::Error> {
     let Args {
         log_level,
@@ -487,6 +757,7 @@ fn main() -> Result<(), anyhow::Error> {
         .with_level(log_level)
         .init()
         .context("failed to initialize simple logger")?;
+    let config = Config::default();
     let mut conn = I3Connection::connect()?;
     let () = match subcommands {
         SubCommands::LogTree(LogTree {}) => {
@@ -496,12 +767,20 @@ fn main() -> Result<(), anyhow::Error> {
         }
         SubCommands::Resize(Resize {
             direction,
+            percentage,
+        }) => {
+            debug!("resize percentage: {percentage:?}");
+            let percentage = percentage.map(|i| i / 100f64);
+            resize(&mut conn, config, direction, percentage)?
+        }
+        SubCommands::ResizeAll(ResizeAll {
+            direction,
             mut percentages,
         }) => {
             for f in percentages.iter_mut() {
                 *f /= 100f64;
             }
-            resize(&mut conn, direction, percentages)?
+            resize_all(&mut conn, direction, percentages)?
         }
         SubCommands::Swap(Swap {
             direction,
@@ -513,7 +792,7 @@ fn main() -> Result<(), anyhow::Error> {
             swap(&mut conn, left, right, noresize)?;
         }
         SubCommands::Rotate(args) => rotate(&mut conn, args)?,
-        SubCommands::Center(Center { direction }) => center(&mut conn, direction)?,
+        SubCommands::Center(Center { direction }) => center(&mut conn, config, direction)?,
     };
     Ok(())
 }
